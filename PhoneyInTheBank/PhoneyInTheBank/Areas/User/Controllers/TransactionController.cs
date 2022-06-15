a66e8cbb662f49cbd9400bea82aded04c5abf36d
i7p7x0 <i7p7x0@gmail.com>
2022-06-15 19:29:33 +0545

    Worked on transaction history

--- PhoneyInTheBank/PhoneyInTheBank/Areas/User/Controllers/TransactionController.cs
+++ PhoneyInTheBank/PhoneyInTheBank/Areas/User/Controllers/TransactionController.cs
@@ -517,10 +517,47 @@ namespace PhoneyInTheBank.Areas.User.Controllers
             return View();
         }
 
-        public IActionResult History()
+        public IActionResult History(int startingNumber, string direction)
         {
 
-            IEnumerable<TransactionHistory> transactions = _unitOfWork.TransactionHistory.GetUserTransactions(x => x.User == User.Identity.Name);
+            int numberOfTransactions = _unitOfWork.TransactionHistory.GetUserTransactions(x => x.User == User.Identity.Name).Count();
+
+            if (direction == "prev")
+            {
+                if (startingNumber - 5 <= 0)
+                {
+                    startingNumber = 0;
+                }
+                else
+                {
+                    startingNumber -= 5;
+                }
+
+            }
+
+            if (direction == "next")
+            {
+                if (startingNumber >= numberOfTransactions)
+                {
+                    startingNumber -= numberOfTransactions - 5;
+                }
+                else if (startingNumber < numberOfTransactions)
+                {
+                    if (numberOfTransactions - startingNumber > 5)
+                    {
+                        startingNumber += 5;
+                    }
+                }
+            }
+
+            ViewData["StartingNumber"] = startingNumber;
+            ViewData["CurrentPage"] = startingNumber / 5 + 1;
+            ViewData["TotalPages"] = numberOfTransactions%5==0 ?(numberOfTransactions / 5) : (numberOfTransactions / 5) + 1;
+            
+            
+
+
+            IEnumerable<TransactionHistory> transactions = _unitOfWork.TransactionHistory.GetUserTransactions(x => x.User == User.Identity.Name).OrderBy(x => x.TransactionDate).Reverse().Skip(startingNumber).Take(5);
             List<TransactionHistoryVM> transactionsVM = new List<TransactionHistoryVM>();
             foreach (var transaction in transactions)
             {
@@ -536,6 +573,8 @@ namespace PhoneyInTheBank.Areas.User.Controllers
                 transactionsVM.Add(vm);
 
             }
+
+
 
             return View(transactionsVM.AsEnumerable());
         }
